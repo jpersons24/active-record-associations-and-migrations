93ac3efc49b0f6a972f2b14ce229bed1a7e8a46b
Jakob Persons <jakobep24@gmail.com>
2020-12-01 08:09:16 -0500

    built associations --> repair genre id in songs table

---
+++ db/seeds.rb
@@ -0,0 +1,35 @@
+#### CREATE ####
+
+
+kanye = Artist.new(name: "Kanye West")
+kanye.save
+
+skrillex = Artist.new(name: "Skrillex")
+skrillex.save
+
+lotus = Artist.new(name: "Lotus")
+lotus.save
+
+rap = Genre.new(name: "Rap")
+rap.save
+
+dubstep = Genre.new(name: "Dubstep")
+dubstep.save
+
+funk = Genre.new(name: "Funk")
+funk.save
+
+
+
+
+#### READ ####
+
+
+
+
+#### UPDATE ####
+
+
+
+
+#### DELETE ####
